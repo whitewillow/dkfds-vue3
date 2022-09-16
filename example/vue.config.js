c42dbb4c602c5b04644bdc5be640905cf2656a64
Mick Neupart <mickneupart@gmail.com>
2022-09-16 13:51:20 +0200

    fix dupe vue instance i example projekt

--- example/vue.config.js
+++ example/vue.config.js
@@ -1,5 +1,10 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack(config) {
+    config.resolve.symlinks(false);
+    config.resolve.alias.set("vue", path.resolve("./node_modules/vue"));
+  },
 });
